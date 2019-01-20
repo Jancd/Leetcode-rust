6e9257a1be6351cc5ffc71cffbe3aed9c1d471bf
Sergey Cheung <sergeychang@gmail.com>
2019-01-20 17:07:55 +0800

    Create 11.Container_With_Most_Water.rs

---
+++ src/11.Container_With_Most_Water.rs
@@ -0,0 +1,28 @@
+impl Solution {
+    pub fn max_area(height: Vec<i32>) -> i32 {
+        use std::cmp;
+        let succ = |x: &mut usize| -> usize {
+            let t = *x;
+            *x += 1;
+            t
+        };
+        let pred = |x: &mut usize| -> usize {
+            let t = *x;
+            *x -= 1;
+            t
+        };
+
+        let (mut l, mut r) = (0, height.len() - 1);
+        let mut max = 0;
+        while l != r {
+            let w = (r - l) as i32;
+            let h = if height[l] > height[r] {
+                height[pred(&mut r)]
+            } else {
+                height[succ(&mut l)]
+            };
+            max = cmp::max(max, h * w);
+        }
+        max
+    }
+}
